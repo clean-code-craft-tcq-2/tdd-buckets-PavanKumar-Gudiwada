6e5a488394375ed1ac25b2491f04aa7d2edd53ef
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-03-12 20:52:33 +0530

    design early test cases

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,30 @@
 #include <string.h>
 #include "test/catch.hpp"
 
-TEST_CASE("test something") {
-//  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
+TEST_CASE("find range in 1 block of continuous range array") {
+  int arr[2] = {4,5};
+  findRanges(arr,2);
+  REQUIRE(ranges[0].lowerLim,4);
+  REQUIRE(ranges[0].upperLim,5);
+  REQUIRE(ranges[0].samplesNumber,2);
+}
+
+TEST_CASE("find range in 1 block of continuous range array") {
+  int arr[4] = {4,5,6,7};
+  findRanges(arr,4);
+  REQUIRE(ranges[0].lowerLim,4);
+  REQUIRE(ranges[0].upperLim,7);
+  REQUIRE(ranges[0].samplesNumber,4);
+}
+
+TEST_CASE("find ranges in 2 blocks continuous range array") {
+  int arr[7] = {3,3,5,4,10,11,12};
+  findRanges(arr,7);
+  REQUIRE(ranges[0].lowerLim,3);
+  REQUIRE(ranges[0].upperLim,5);
+  REQUIRE(ranges[0].samplesNumber,4);
+  
+  REQUIRE(ranges[1].lowerLim,10);
+  REQUIRE(ranges[1].upperLim,12);
+  REQUIRE(ranges[1].samplesNumber,3);
 }
