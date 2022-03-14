5e48195ef591fffa7086b8bbe7737b445cb7f286
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-03-14 19:43:38 +0530

    edits to test cases

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -2,10 +2,15 @@
 
 #include <string.h>
 #include "test/catch.hpp"
+#include "ranges.h"
+#include "sort.h"
 
 TEST_CASE("find range in 1 block of continuous range array") {
   int arr[2] = {4,5};
-  findRanges(arr,2);
+  struct ranges* ranges;
+  
+  ranges = findRanges(arr,2);
+  
   REQUIRE(ranges[0].lowerLim,4);
   REQUIRE(ranges[0].upperLim,5);
   REQUIRE(ranges[0].samplesNumber,2);
@@ -13,7 +18,10 @@ TEST_CASE("find range in 1 block of continuous range array") {
 
 TEST_CASE("find range in 1 block of continuous range array") {
   int arr[4] = {4,5,6,7};
-  findRanges(arr,4);
+  struct ranges* ranges;
+  
+  ranges = findRanges(arr,4);
+  
   REQUIRE(ranges[0].lowerLim,4);
   REQUIRE(ranges[0].upperLim,7);
   REQUIRE(ranges[0].samplesNumber,4);
@@ -21,7 +29,10 @@ TEST_CASE("find range in 1 block of continuous range array") {
 
 TEST_CASE("find ranges in 2 blocks continuous range array") {
   int arr[7] = {3,3,5,4,10,11,12};
-  findRanges(arr,7);
+  struct ranges* ranges;
+  
+  ranges = findRanges(arr,7);
+  
   REQUIRE(ranges[0].lowerLim,3);
   REQUIRE(ranges[0].upperLim,5);
   REQUIRE(ranges[0].samplesNumber,4);
