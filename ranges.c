d4a20cb93453da835bbddd1536ec12cf1c0542f3
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-03-16 21:21:43 +0530

    reduce cnn

--- ranges.c
+++ ranges.c
@@ -4,7 +4,23 @@
 #include "ranges.h"
 
 struct ranges finalRanges[5];
-int numberOfRanges = 0;
+static int numberOfRanges = 0;
+
+static void categoriseValue(const void* values, int * controlVariable)
+{
+	if((((int*)values)[*controlVariable] >= finalRanges[numberOfRanges].upperLim) && (((((int*)values)[*controlVariable-1]+1) == ((int*)values)[*controlVariable]) || ((((int*)values)[*controlVariable-1]) == ((int*)values)[*controlVariable])) )
+	{
+		finalRanges[numberOfRanges].upperLim = ((int*)values)[*controlVariable];
+		finalRanges[numberOfRanges].numberOfElements += 1;
+	}else if ((((int*)values)[*controlVariable-1]+1) != ((int*)values)[*controlVariable])
+	{
+		numberOfRanges++;
+		finalRanges[numberOfRanges].lowerLim = ((int*)values)[*controlVariable];
+		finalRanges[numberOfRanges].upperLim = ((int*)values)[*controlVariable+1];
+		finalRanges[numberOfRanges].numberOfElements = 2;
+		*controlVariable = *controlVariable+1;//increment i by 1 here and then immediate increment will be done by for loop
+	}
+}
 
 struct ranges* findRanges(const void* values, int numberOfValues)
 {
@@ -18,18 +34,7 @@ struct ranges* findRanges(const void* values, int numberOfValues)
 	
 	for(i = 2; i < numberOfValues; i++)
 	{
-		if((((int*)values)[i] >= finalRanges[numberOfRanges].upperLim) && (((((int*)values)[i-1]+1) == ((int*)values)[i]) || ((((int*)values)[i-1]) == ((int*)values)[i])) )
-		{
-			finalRanges[numberOfRanges].upperLim = ((int*)values)[i];
-			finalRanges[numberOfRanges].numberOfElements += 1;
-		}else if ((((int*)values)[i-1]+1) != ((int*)values)[i])
-		{
-			numberOfRanges++;
-			finalRanges[numberOfRanges].lowerLim = ((int*)values)[i];
-			finalRanges[numberOfRanges].upperLim = ((int*)values)[i+1];
-			finalRanges[numberOfRanges].numberOfElements = 2;
-			i = i+1;//increment i by 1 here and then immediate increment will be done by for loop
-		}
+		categoriseValue(values,&i);
 	}
 	
 	return finalRanges;
