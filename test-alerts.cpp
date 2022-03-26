9ab71c6b3f55fa4de903fbab3cd6028e0c829a8e
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2022-03-26 14:01:18 +0530

    add tests for new functionality

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -47,3 +47,54 @@ TEST_CASE("find ranges in 2 blocks continuous range array") {
   REQUIRE((ranges[1].upperLim) == 12);
   REQUIRE((ranges[1].numberOfElements) == 3);
 }
+
+TEST_CASE("check if convert function works") {
+  int analogValue = 1146, digitalValue;
+  
+  digitalValue = currentConvereterA2D(analogValue);
+  REQUIRE(digitalValue == 3);
+  
+  analogValue = 3400;
+  digitalValue = currentConvereterA2D(analogValue);
+  REQUIRE(digitalValue == 8);
+}
+
+TEST_CASE("check if convert array function works") {
+  int arr[7] = {3400,0,1146,4095,500,249,1238};
+  
+  currentArrayConvertersA2D(arr,7);
+  REQUIRE(arr[0] == 8);
+  REQUIRE(arr[1] == 0);
+  REQUIRE(arr[2] == 3);
+  REQUIRE(arr[3] == 4095);
+  REQUIRE(arr[4] == 1);
+  REQUIRE(arr[5] == 1);
+  REQUIRE(arr[6] == 3);
+}
+
+TEST_CASE("end to end test case, convert analog to amp value then find ranges") {
+  int arr[7] = {1146,1238,2047,1638,4094,4503,4913};//{3,3,5,4,10,11,12}
+  struct ranges* ranges;
+  
+  currentArrayConvertersA2D(arr,7);
+  REQUIRE(arr[0] == 3);
+  REQUIRE(arr[1] == 3);
+  REQUIRE(arr[2] == 5);
+  REQUIRE(arr[3] == 4);
+  REQUIRE(arr[4] == 10);
+  REQUIRE(arr[5] == 11);
+  REQUIRE(arr[6] != 12);//for a 12 bit number 4913 will cause an overflow ideally and hence this should be !=12
+  
+  ranges = findRanges(arr,7);
+  
+  printRanges(ranges);
+  
+  REQUIRE((ranges[0].lowerLim) == 3);
+  REQUIRE((ranges[0].upperLim) == 5);
+  REQUIRE((ranges[0].numberOfElements) == 4);
+  
+  REQUIRE((ranges[1].lowerLim) == 10);
+  REQUIRE((ranges[1].upperLim) == 12);
+  REQUIRE((ranges[1].numberOfElements) == 3);
+  
+}
